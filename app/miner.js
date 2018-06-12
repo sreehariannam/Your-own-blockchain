169987716aafc0976dbbb88305eb9fbd57be6243
Sreehari Annam <sreehari.annam@gmail.com>
2018-06-12 07:59:44 +0530

    first commit

---
+++ app/miner.js
@@ -0,0 +1,21 @@
+class Miner {
+  constructor(blockchain, transactionPool, wallet, p2pServer) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+    this.wallet = wallet;
+    this.p2pServer = p2pServer;
+  }
+
+  mine() {
+    const validTransactions = this.transactionPool.validTransactions();
+    validTransactions.push(
+      Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet())
+    );
+    const block = this.blockchain.addBlock(validTransactions);
+    this.p2pServer.syncChains();
+    this.transactionPool.clear();
+    this.p2pServer.broadcastClearTransactions();
+  }
+}
+
+module.exports = Miner;
